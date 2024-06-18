b7d7da26b58af5376e3c2a9011b5238953d0535d
teshager <teshager8922@gmail.com>
2024-06-18 10:59:15 +0300

    populate the grid with all 256 elements

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const grid= document.querySelector('.grid')
-for(let i=0;i<16;i++){
+for(let i=0;i<256;i++){
   let Element=document.createElement('div');
   Element.classList.add('gridElement')
    grid.appendChild(Element);
