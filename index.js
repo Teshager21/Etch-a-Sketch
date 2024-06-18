ecb71172ac9f3a73919f150aeeb78e8384fc7d1f
teshager <teshager8922@gmail.com>
2024-06-18 19:50:27 +0300

    increase darkenning of a grid based the repetition of brush

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const grid= document.querySelector('.grid')
 const handleHover=(e)=>{
     e.target.classList.add('hover')
+    let opacity =parseInt(e.target.getAttribute('data-opacity'));
+    e.target.setAttribute('data-opacity', opacity+1)
+    e.target.style.opacity=opacity*0.1;
+    // e.target.style.opacity=0.3;
+    // console.log('current opacity is: ',currentOpacity)
+   
+    console.log( e.target.getAttribute('data-opacity'));
 }
-// const size = Input("Input the number of squares per side");
 for(let i=0;i<256;i++){
   let Element=document.createElement('div');
   Element.classList.add('gridElement')
   Element.addEventListener('mouseover',handleHover);
+  Element.setAttribute('data-opacity',0)
   grid.appendChild(Element);
+
 
 }
 
+const setGridSize=(e)=>{
+    // alert(e.target)
+    let size=input('input sth');
+}
+
+document.getElementById('btnSize').addEventListener('click',setGridSize)
 
