0186048f206e711cdf947f0e8c834ee4da3921af
teshager <teshager8922@gmail.com>
2024-06-18 10:49:48 +0300

    add 15 more elements to the grid

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const grid= document.querySelector('.grid')
-// for(let i=0;i>16;i++){
-    let Element=document.createElement('div')
-     Element.classList.add('gridElement')
-    grid.appendChild(Element);
+for(let i=0;i<16;i++){
+  let Element=document.createElement('div');
+  Element.classList.add('gridElement')
+   grid.appendChild(Element);
 
-// }
-
-alert('hello')
+}
