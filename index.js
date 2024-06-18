2a78aa0a1257e7059bbecec54a0a80016505e3a5
teshager <teshager8922@gmail.com>
2024-06-18 11:13:15 +0300

    add a graying class to grid element  on mouseover event

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const grid= document.querySelector('.grid')
+const handleHover=(e)=>{
+    e.target.classList.add('hover')
+}
 for(let i=0;i<256;i++){
   let Element=document.createElement('div');
   Element.classList.add('gridElement')
+  Element.addEventListener('mouseover',handleHover);
    grid.appendChild(Element);
 
 }
+
+
