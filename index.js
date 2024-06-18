7d78b8d2e533313bcd9bcae64c586c0e2eb1e988
teshager <teshager8922@gmail.com>
2024-06-18 16:42:55 +0300

    add a button for triggering prompt

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const grid= document.querySelector('.grid')
 const handleHover=(e)=>{
     e.target.classList.add('hover')
 }
+// const size = Input("Input the number of squares per side");
 for(let i=0;i<256;i++){
   let Element=document.createElement('div');
   Element.classList.add('gridElement')
   Element.addEventListener('mouseover',handleHover);
-   grid.appendChild(Element);
+  grid.appendChild(Element);
 
 }
 
