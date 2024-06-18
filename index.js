c68a3b3425ac9dc73d5abd6bf80854fa13756ffb
teshager <teshager8922@gmail.com>
2024-06-18 10:33:16 +0300

    add one grid element to the grid

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const grid= document.querySelector('.grid')
 // for(let i=0;i>16;i++){
     let Element=document.createElement('div')
      Element.classList.add('gridElement')
-    grid.appendChild(gridElement);
+    grid.appendChild(Element);
+
 // }
 
 alert('hello')
